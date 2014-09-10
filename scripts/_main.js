0114975c1da7523208fead2ac77ec8cb2e4b6c9c
dvdrtrgn <dvdrtrgn@gmail.com>
2014-09-10 15:02:32 -0500

    Squashed commit of the following:
    commit 276dcf5cf91c06315adf98d265b835f672a60169
    Author: dvdrtrgn <dvdrtrgn@gmail.com>
    Date:   Wed Sep 10 14:54:45 2014 -0500
    
        make little image tweaks
    
    commit a9b4201ae0d5d3bdeca02d7d11d7037cc2c55c59
    Author: dvdrtrgn <dvdrtrgn@gmail.com>
    Date:   Wed Sep 10 14:54:18 2014 -0500
    
        start load fonts
    
    commit a679f2ec8fd692b5afb1f4ba20820bcdbaef738b
    Author: dvdrtrgn <dvdrtrgn@gmail.com>
    Date:   Wed Sep 10 14:53:48 2014 -0500
    
        add english text
    
    commit 972b0da3936145934b3a73ec6e12c068eaf729ca
    Author: dvdrtrgn <dvdrtrgn@gmail.com>
    Date:   Wed Sep 10 12:45:38 2014 -0500
    
        tweak for mini screens
    
    commit 71aac58e54b2cabd9a2c28507e09bc11be4ff5a4
    Author: dvdrtrgn <dvdrtrgn@gmail.com>
    Date:   Wed Sep 10 12:24:53 2014 -0500
    
        reorg sass
    
    commit 130ea2f85a00ec14b12b155c6c56ff0f06a74bb4
    Author: dvdrtrgn <dvdrtrgn@gmail.com>
    Date:   Wed Sep 10 11:48:38 2014 -0500
    
        tweak header

--- scripts/_main.js
+++ scripts/_main.js
@@ -16,6 +16,10 @@ var Main = (function ($, G, U) { // IIFE
             body = $('body');
             html = $('html');
 
+            if (jsView.mobile.agent()) {
+                html.addClass('mini');
+                jsView.mobile.addBug();
+            }
             if (shape === 'landscape') {
                 body.removeClass('wide').addClass('high');
             } else if (shape === 'portrait') {
@@ -47,9 +51,19 @@ var Main = (function ($, G, U) { // IIFE
             }, 333);
         });
 
-        $('button').on('click', function () {
-            W.open('http://www.prizelabs.com/atupasion', 'offsite');
+        $('button').on('click', function (evt) {
+            var me = $(evt.target);
+
+            W.open(me.data('url'), 'offsite');
         });
+
+        $('.grad').on('click', function () {
+            if (html.is('.esp')) {
+                html.removeClass('esp').addClass('eng');
+            } else {
+                html.removeClass('eng').addClass('esp');
+            }
+        })
     }
 
     /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */

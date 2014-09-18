091c27028e4bd1b706337c36be84864ac6635095
dvdrtrgn <dvdrtrgn@gmail.com>
2014-09-18 18:33:41 -0500

    make parity with harbor

--- tasks/options/connect.js
+++ tasks/options/connect.js
@@ -3,21 +3,21 @@ module.exports = {
     // CONNECT
     // https://github.com/gruntjs/grunt-contrib-connect
 
+    options: {
+        livereload: 7043,
+        port: 8043,
+    },
     base: {
         options: {
             base: ['app', '.', '../..'],
             open: false,
-            livereload: 7043,
-            port: 8043,
         },
     },
     full: {
         options: {
             base: ['app', '.', '../..'],
             //hostname: 'localhost', // Change this to '0.0.0.0' to access the server from outside
-            open: true,
-            livereload: 7043,
-            port: 8043,
+            open: 'http://localhost:8043', // target url to open
         },
     },
 };

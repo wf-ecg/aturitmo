49a46dc6846ffb61dfad8760a1db428d01603581
dvdrtrgn <dvdrtrgn@gmail.com>
2014-10-08 01:06:43 -0500

    simplify grunting

--- tasks/options/watch.js
+++ tasks/options/watch.js
@@ -3,34 +3,27 @@ module.exports = {
     // WATCH
     // https://github.com/gruntjs/grunt-contrib-watch
 
-    // revelation
-    // grunt is file centric rather than task???
-    // make sure rules don't double match files
-    lib: {
-        files: ['libs/*.js'],
-        tasks: ['jshint:precat', 'concat:base'],
+    options: {
+        debounceDelay: 3333,
     },
-    src: {
-        files: ['scripts/*.js'],
+    cat: {
+        files: ['libs/*.js', 'scripts/*.js'],
         tasks: ['jshint:precat', 'concat:base'],
     },
     css: {
         files: ['scss/**/*.scss'],
         tasks: ['sass:base'],
     },
-    html: {
-        files: ['app/**/*.html'],
-    },
     reloads: {
         options: {
             livereload: 7043,
         },
-        files: ['app/**/*', '!**/*.map'],
-        tasks: ['jshint:precat', 'sync:base'],
+        files: ['app/**/*', '!app/**/*.map'],
+        tasks: ['jshint:postcat', 'sync:base'],
     },
     warn: {
         options: { reload: !false, },
         files: ['Gruntfile.js', 'tasks/**/*'],
-        tasks: ['foobar'],
+        tasks: ['warning'],
     },
 };

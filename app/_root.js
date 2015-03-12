e270056b8e39abda0e32f319d86d604a2810a1bd
dvdrtrgn <dvdrtrgn@gmail.com>
2015-03-12 17:59:23 -0500

    standardize reload and bounce

--- app/_root.js
+++ app/_root.js
@@ -83,7 +83,7 @@ W.ROOT = ({
         if (C && C.groupCollapsed) {
             C.groupEnd();
         }
-        delete this.reload;
+        delete this.reload; /// retain reload if async needed
         delete this.loaded;
     },
     init: function () {
--- scripts/scroller.js
+++ scripts/scroller.js
@@ -226,7 +226,7 @@ var Scroller = (function ($, G, U) { // IIFE
     /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
     function _init() {
-        if (self.inited(true)) {
+        if (self.isInited(true)) {
             return null;
         }
         Df.inits();
